5253dcabfd2fb95d159410f73592f55658bd80e5
retr0yse <122714764+retr0yse@users.noreply.github.com>
2024-10-27 13:57:13 +0400

    Add files via upload

---
+++ Project14.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	//Гравитационная постоянная
+	const double G = 6.672E-11;
+	//Масса земли
+	const double M = 5.96E24;
+	//Радиус земли
+	const double R = 6.37E6;
+	//Число pi
+	const double pi = 3.1415;
+	//Период и высота орбиты
+	double T=0.0, h	= 0.0;
+	//Ввод периода в часах
+	cout << "Enter T = ";
+	cin >> T;
+	//Перевод часов в секунды:
+	T = T * 3600;
+	//Определение высоты:
+	h = pow(G * M * T * T / 4 / pi / pi, (double)1 / 3) - R;
+	//Перевод в километры
+	h = h / 1000;
+	cout << "Height h=" << h << "km\n";
+	return 0;
+}
